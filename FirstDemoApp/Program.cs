cd5ac59e0aa5586f175d4ab988602f7bd5b160bd
amiri <amiri@LAPTOP-LOR3J9D3>
2021-06-03 14:39:45 -0400

    Add project files.

---
+++ FirstDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
